4b62843c2c827188b029bdc2981b7702e00f5ff9
KanikaRobin <58131128+KanikaRobin@users.noreply.github.com>
2020-04-14 19:50:37 +0530

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -5,22 +5,21 @@ class Pig extends BaseClass {
     this.Visiblity = 255;
   }
 
- display(){
-   console.log(this.body.speed);
-   if(this.body.speed < 3){
+display(){
+  console.log(this.body.speed);
+  super.display();
+  if(this.body.speed<3){
     super.display();
-   }
-   else{
-     World.remove(world, this.body);
-     push();
-     this.Visiblity = this.Visiblity - 5;
-     tint(255,this.Visiblity);
-     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-     pop();
-   }
-   
- }
-
-
+  
+  }
+  else {
+    World.remove(world,this.body);
+    push();
+    this.Visibility=this.Visibility-5;
+    tint(255,this.Visibility);
+    image(this.image,this.position.x,this.position.y,50,50);
+    pop();
+  }
+}
 
 };
